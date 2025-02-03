a555447cf60570ee9764052ec5a2362a494877af
fathima <fathimanezrin2001@gmail.com>
2025-02-03 13:26:40 +0530

    commit

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -4,20 +4,24 @@ import useCategory from "../hooks/useCategory";
 import { Link } from "react-router-dom";
 
 const Categories = () => {
-    const categories = useCategory();
+  const categories = useCategory();
   return (
     <Layout title={"All Categories"}>
-      <div className=" row container justify-center" style={{ margin: "120px" }}>
+      <div
+        className=" row container justify-center"
+        style={{ margin: "120px" }}
+      >
         <div className="row">
-            {categories.map((category) => (
-                <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={category._id}>
-                    <Link className="btn cat-btn text-center p-3 border-2 border-transparent outline outline-2 outline-gray-500 rounded-md hover:bg-gray-100" to={`/categories/${category.slug}`}>
-                    {category.name}
-                    </Link>
-                
-                </div>
-            ))}
-            
+          {categories.map((category) => (
+            <div className="col-md-4 mt-5 mb-3 gx-3 gy-3" key={category._id}>
+              <Link
+                className="btn cat-btn text-center p-3 border-2 border-transparent outline outline-2 outline-gray-500 rounded-md hover:bg-gray-100"
+                to={`/categories/${category.slug}`}
+              >
+                {category.name}
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </Layout>
